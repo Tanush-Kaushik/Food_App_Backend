b9cdede88c635b2a973c6f5eeb35b388d4517b09
Tanush-Kaushik <141478739+Tanush-Kaushik@users.noreply.github.com>
2024-07-10 19:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ config({path:'./config.env'})
 const app = express()
 const port = process.env.PORT
 
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin',process.env.FRONTEND_URL)
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin,X-Requested-With,Content-type,Accept'
-//     )
-//     next()
-// })
-app.use(cors())
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin',process.env.FRONTEND_URL)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-type,Accept'
+    )
+    next()
+})
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
